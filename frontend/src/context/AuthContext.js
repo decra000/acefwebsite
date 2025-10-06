1b5fea3b49e3ff830b771af283b502c67610e1a0
decra000 <165248774+decra000@users.noreply.github.com>
2025-10-06 23:56:08 +0300

    BUILD PROTECTED

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { API_URL, STATIC_URL } from '../config';
+import { API_URL } from '../config';
 
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
